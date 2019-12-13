1771e7276e062620da0b3a3d472f8dab9ca11398
Cláudio Madureira <claudiomsilvaf@gmail.com>
2019-12-12 21:28:39 -0300

    add brazilian phone formatter

--- source/string/String-Formatter.swift
+++ source/string/String-Formatter.swift
@@ -63,6 +63,45 @@ public extension String {
         zipcode.insert("-", at: 8 - 3)
         return zipcode.joined()
     }
+    
+    /// Format a brazilian phone number to it's respective number count representative way.
+    /// Examples:
+    ///     "84248008" -> "8424-8008"
+    ///     "984248008" -> "9 8424-8008"
+    ///     "3184248008" -> "(31) 8424-8008"
+    ///     "31984248008" -> "(31) 9 8424-8008"
+    ///
+    /// - Returns: Formatted brazilian phone number if the string contains the right amount of number characters to do so.
+    func formattedBrazilianPhone() -> String? {
+        let numbers = self.compactMap { Int(String($0)) }
+        guard numbers.count > 7 else { return nil }
+        var formattedPhone = numbers.map { String($0) }
+        switch formattedPhone.count {
+        case 8:
+            formattedPhone.insert("-", at: 4)
+            return formattedPhone.joined()
+        case 9:
+            formattedPhone.insert("-", at: 5)
+            formattedPhone.insert(" ", at: 1)
+            return formattedPhone.joined()
+        case 10:
+            formattedPhone.insert("-", at: 6)
+            formattedPhone.insert(" ", at: 2)
+            formattedPhone.insert(")", at: 2)
+            formattedPhone.insert("(", at: 0)
+            return formattedPhone.joined()
+        case 11:
+            formattedPhone.insert("-", at: 7)
+            formattedPhone.insert(" ", at: 3)
+            formattedPhone.insert(" ", at: 2)
+            formattedPhone.insert(")", at: 2)
+            formattedPhone.insert("(", at: 0)
+            return formattedPhone.joined()
+        default:
+            return nil
+        }
+    }
+
 
     
 }
