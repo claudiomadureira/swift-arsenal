1ee3026aa3d15b9b3ef7ff89f420da85f3500c7f
Cláudio Madureira <claudiomsilvaf@gmail.com>
2020-01-27 15:06:28 -0300

    add `formattedCPF` method tests

--- example/SwiftArsenal/SwiftArsenalTests/StringFormaterTests.swift
+++ example/SwiftArsenal/SwiftArsenalTests/StringFormaterTests.swift
@@ -32,5 +32,29 @@ class StringFormatterTests: XCTestCase {
         let date: String = "1995-04-10"
         XCTAssertEqual(date.rawNumbers(), "19950410")
     }
+    
+    // MARK: - Method `FormattedCPF`
+    
+    func testFormattedCPFOnExpectedNumberLength() {
+        let cleanDocument: String = "01234567890"
+        XCTAssertEqual(cleanDocument.formattedCPF(), "012.345.678-90")
+        let formattedDocument: String = "012.345.678-90"
+        XCTAssertEqual(formattedDocument.formattedCPF(), "012.345.678-90")
+    }
+    
+    func testFormattedCPFOnUnexpectedNumberLength() {
+        let example1: String = "012345678901"
+        XCTAssertEqual(example1.formattedCPF(), nil)
+        let example2: String = "0123456789"
+        XCTAssertEqual(example2.formattedCPF(), nil)
+        let example3: String = "012345678"
+        XCTAssertEqual(example3.formattedCPF(), nil)
+        let example4: String = "01234567"
+        XCTAssertEqual(example4.formattedCPF(), nil)
+        let example5: String = "0123456"
+        XCTAssertEqual(example5.formattedCPF(), nil)
+        // It continues for any length that's different from 11.
+    }
+    
 
 }
