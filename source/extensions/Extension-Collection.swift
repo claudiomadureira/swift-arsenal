9b1c98f7710e72ac016f9b9b2de13d9cd52cb712
Claudio Madureira <claudiosilva@ciandt.com>
2020-02-10 15:05:49 -0300

    add collection extension & gitignore

---
+++ source/extensions/Extension-Collection.swift
@@ -0,0 +1,15 @@
+//
+//  Extension-Collection.swift
+//
+//  Created by Claudio Madureira on 19/12/19.
+//
+
+import Foundation
+
+public extension Collection {
+    
+    var hasElements: Bool {
+        return !self.isEmpty
+    }
+    
+}
