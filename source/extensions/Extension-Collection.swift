03f20a1206d43b2ff61a6e201e26fea43e4298bb
Claudio Madureira <claudiosilva@ciandt.com>
2020-02-12 08:18:15 -0300

    documenting collection extensions

--- source/extensions/Extension-Collection.swift
+++ source/extensions/Extension-Collection.swift
@@ -8,10 +8,12 @@ import Foundation
 
 public extension Collection {
     
+    /// Identifies collection has a single element or more. (isEmpty opposite)
     var hasElements: Bool {
         return !self.isEmpty
     }
     
+    /// Safely get an element inside an array. This avoids `index out of range`.
     subscript(safe index: Index) -> Element? {
         return indices.contains(index) ? self[index] : nil
     }
